f47b8a163d2cfed28001e4a75649a47866106dfc
pablo9306 <62298647+pablo9306@users.noreply.github.com>
2020-03-26 20:40:10 -0300

    Restaurant

---
+++ EjercicioClase/EjercicioClase/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Restaurant Pablin = new Restaurant(5, true);
+            Console.WriteLine("Ingresar la cantidad de mesas a pedir:");
+            int mesaspedidas = int.Parse(Console.ReadLine());
+            Pablin.CupoMesas(mesaspedidas);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EjercicioClase/EjercicioClase/Restaurant.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase
+{
+    class Restaurant
+    {
+        private int _cantMesas;
+        private string _direccion;
+        private bool _estaAbierto;
+
+        public int cantMesas
+        {
+            get
+            {
+                return this._cantMesas;
+            }
+            set
+            {
+                this._cantMesas = value;
+            }
+        }
+
+        public string direccion
+        {
+            get
+            {
+                return this._direccion;
+            }
+            set
+            {
+                this._direccion = value;
+            }
+        }
+
+        public Restaurant(int cantMesas, bool estaAbierto)
+        {
+            this._cantMesas = cantMesas;
+            this._estaAbierto = estaAbierto;
+        }
+
+        public bool CupoMesas(int mesasreservar)
+        {
+            if (this._estaAbierto)
+            {
+                if (this._cantMesas <= mesasreservar)
+                {
+                    Console.WriteLine("Mesas a reservar excedido");
+                    return false;
+                }
+                Console.WriteLine("Se reservaron " + mesasreservar + " mesas");
+                this._cantMesas = (this._cantMesas - mesasreservar);
+                return true;
+            }
+            Console.WriteLine("Restaurant cerrado");
+            return false;
+        }
+     }
+}
